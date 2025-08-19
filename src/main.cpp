ebb6b2fa5099708f676baa7ffad7d97248b8a74b
johnpgr <jp.greidinger@hotmail.com>
2025-08-19 09:33:36 -0300

    chores

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,14 @@
 #include "cli.h"
 
 int main(int argc, char* argv[]) {
-    constexpr usize BUFSIZE = MB(4);
-    u8* buffer = (u8*)malloc(BUFSIZE);
-    FixedBufferAllocator fba = FixedBufferAllocator::init(&buffer, BUFSIZE);
-    Allocator allocator = fba.allocator();
-    defer {
-        fba.deinit();
-        free(buffer);
-    };
+    ArenaAllocator arena = ArenaAllocator::init(PageAllocator::init(), 4096, MB(8));
+    Allocator allocator = arena.allocator();
+    defer { arena.deinit(); };
 
-    // CLIParser parser = CLIParser::init(allocator, "Bible Reader");
-    // defer { parser.deinit(allocator); };
-    //
-    // parser.parse(argc, argv);
+    CLIParser parser = CLIParser::init(allocator, "Bible Reader");
+    defer { parser.deinit(allocator); };
+
+    parser.parse(argc, argv);
 
     return 0;
 }
